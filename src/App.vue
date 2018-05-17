b0db98ca8796d75d8f418675a33fb96b911f623c
Leewgg <wangle1314@outlook.com>
2018-05-17 23:13:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import store from './vuex/store'
+
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="blog-post">
+    <h3>{{ post.title }}</h3>
+    <button v-on:click="$emit('enlarge-text')">
+      放大文本
+    </button>
+    <div v-html="post.content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: ['post']
+}
+</script>
+
+<style scoped>
+
+</style>
